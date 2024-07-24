e59bc99106a32937fae2ae3528bbe494efe75f00
Don <496156+nikolat@users.noreply.github.com>
2024-07-24 16:42:10 +0900

    add metadata of reaction event

--- src/lib/components/Reaction.svelte
+++ src/lib/components/Reaction.svelte
@@ -8,46 +8,49 @@
   $: emojiTag = ev.tags.find(tag => tag[0] === 'emoji');
 </script>
 
-<span class="makibishi-unit">
-  {#if profiles.has(ev.pubkey)}
+<span class="makibishi-reaction"
+  data-nevent={nip19.neventEncode({...ev, author: ev.pubkey})}
+  data-npub={nip19.npubEncode(ev.pubkey)}
+  data-created-at={ev.created_at}
+  >{#if profiles.has(ev.pubkey)}
     {@const prof = profiles.get(ev.pubkey)}
     {@const obj = JSON.parse(prof?.content ?? '{}')}
-    {@const npub = nip19.npubEncode(ev.pubkey) }
-    {@const name = obj.name ?? '' }
-    <span class="makibishi-content">{#if ev.content === `:${emojiTag?.at(1) ?? ''}:`}<img src={emojiTag?.at(2)} alt={ev.content} title={ev.content} />{:else}{ ev.content }{/if}</span
+    {@const npub = nip19.npubEncode(ev.pubkey)}
+    {@const name = obj.name ?? ''}
+    <span class="makibishi-content">{#if ev.content === `:${emojiTag?.at(1) ?? ''}:`}<img src={emojiTag?.at(2)} alt={ev.content} title={ev.content} />{:else}{ev.content}{/if}</span
     ><a class="makibishi-link" href="{urlToLinkEvent}/{npub}" target="_blank" rel="noopener noreferrer"
       ><img class="makibishi-profile-picture" src={obj.picture ?? getRoboHashURL(ev.pubkey)} alt="@{name}" title="@{name}"
     /></a
   >{:else}
-    <span class="makibishi-content">{#if ev.content === `:${emojiTag?.at(1) ?? ''}:`}<img src={emojiTag?.at(2)} alt={ev.content} title={ev.content} />{:else}{ ev.content }{/if}</span
+    <span class="makibishi-content">{#if ev.content === `:${emojiTag?.at(1) ?? ''}:`}<img src={emojiTag?.at(2)} alt={ev.content} title={ev.content} />{:else}{ev.content}{/if}</span
   >{/if
 }</span>
 
 <style>
-  span.makibishi-unit a {
+  span.makibishi-reaction a {
     text-decoration: none;
   }
-  span.makibishi-unit {
+  span.makibishi-reaction {
     position: relative;
   }
-  span.makibishi-unit > a.makibishi-link {
+  span.makibishi-reaction > a.makibishi-link {
     position: absolute;
     bottom: 16px;
     left: 0;
     visibility: hidden;
   }
-  span.makibishi-unit:hover > a.makibishi-link {
+  span.makibishi-reaction:hover > a.makibishi-link {
     visibility: visible;
   }
-  span.makibishi-unit > span.makibishi-content {
+  span.makibishi-reaction > span.makibishi-content {
     display: inline-block;
     min-width: 16px;
   }
-  span.makibishi-unit > span.makibishi-content > img {
+  span.makibishi-reaction > span.makibishi-content > img {
     height: 16px;
     vertical-align: bottom;
   }
-  span.makibishi-unit > a.makibishi-link > img {
+  span.makibishi-reaction > a.makibishi-link > img {
     width: 16px;
     height: 16px;
     border-radius: 10%;

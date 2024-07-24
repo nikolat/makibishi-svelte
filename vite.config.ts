f6dbe8df8ef97fba72d44ff2365426a2cae0054e
Don <496156+nikolat@users.noreply.github.com>
2024-07-24 09:59:27 +0900

    combine a css file into a js file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    cssInjectedByJsPlugin(),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/makibishi.js`,
+      }
+    }
+  },
 })

170892625d374b753315d6f0b813f39d3198f461
Don <496156+nikolat@users.noreply.github.com>
2024-07-28 14:17:22 +0900

    update

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,12 @@
 import { npubEncode } from 'nostr-tools/nip19';
-export const reactionEventKind = 7;
+export const reactionEventKind = 17;
 export const expansionThreshold = 5;
-export const urlToLinkEvent = 'https://nostter.app';
-export const defaultRelays = ['wss://relay.mymt.casa/'];
+export const urlToLinkEvent = 'https://njump.me';
+export const defaultRelays = [
+  'wss://relay.nostr.band/',
+  'wss://nos.lol/',
+  'wss://relay.damus.io/',
+];
 export const profileRelays = [
   'wss://purplepag.es/',
   'wss://directory.yabu.me/',

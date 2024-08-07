ff04428fb2726cafe0abb099054dc14f1e1fc3bb
Don <496156+nikolat@users.noreply.github.com>
2024-08-07 14:35:00 +0900

    anonymous function

--- src/main.ts
+++ src/main.ts
@@ -2,37 +2,39 @@ import { SimplePool } from 'nostr-tools/pool';
 import { generateSecretKey } from 'nostr-tools/pure';
 import App from './lib/components/App.svelte';
 
-const pool = new SimplePool();
-const anonymousSeckey = generateSecretKey();
-
-const initTarget = (element: HTMLElement): void => {
-  if (!element.hasChildNodes()) {
-    new App({
-      target: element,
-      props: {
-        element,
-        pool,
-        anonymousSeckey,
-      },
-    });
-  }
-};
-
-const initTargets = (selector?: string): void => {
-  document
-    .querySelectorAll<HTMLElement>(selector || '.makibishi')
-    .forEach((element) => {
-      initTarget(element);
-    });
-};
-
 declare global {
   var makibishi: object;
 }
 
-if (typeof window === 'object') {
-  window.makibishi = { initTarget, initTargets };
-  document.addEventListener('DOMContentLoaded', () => {
-    initTargets();
-  });
-}
+(() => {
+  const pool = new SimplePool();
+  const anonymousSeckey = generateSecretKey();
+
+  const initTarget = (element: HTMLElement): void => {
+    if (!element.hasChildNodes()) {
+      new App({
+        target: element,
+        props: {
+          element,
+          pool,
+          anonymousSeckey,
+        },
+      });
+    }
+  };
+
+  const initTargets = (selector?: string): void => {
+    document
+      .querySelectorAll<HTMLElement>(selector || '.makibishi')
+      .forEach((element) => {
+        initTarget(element);
+      });
+  };
+
+  if (typeof window === 'object') {
+    window.makibishi = { initTarget, initTargets };
+    document.addEventListener('DOMContentLoaded', () => {
+      initTargets();
+    });
+  }
+})();
